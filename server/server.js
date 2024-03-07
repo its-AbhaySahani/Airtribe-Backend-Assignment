89f0a12f5e98de257b3056b8b75ab33f55ecbdbb
Abhay Sahani <abhay.cs.777@gmail.com>
2024-03-07 23:13:33 +0530

    setup server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const app = express();
 const cors=require("cors");
 
@@ -5,14 +6,17 @@ const cors=require("cors");
 
 app.use(cors())
 app.use(express.json())
-app.use(require('./router/auth'));
+// app.use(require('./router/auth'));
 
+app.get('/', (req, res) => {
+    res.send('Heyy, Welcome to the Airtribe server!');
+    });
 
 // Start the server
 const PORT = 5000;
 
-connectDb().then(() => {
+// connectDb().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
   });
-});
+// });
