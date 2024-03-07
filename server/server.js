b5daf5447efa503b74932ebae7cb2c71e485653a
Abhay Sahani <abhay.cs.777@gmail.com>
2024-03-07 22:54:08 +0530

    initiated server on port 5000

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const app = express();
+const cors=require("cors");
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(require('./router/auth'));
+
+
+// Start the server
+const PORT = 5000;
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+});
